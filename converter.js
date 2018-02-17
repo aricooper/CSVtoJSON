35370490758007112839edc5a17da3e89ca16688
Ari Kory Cooper <arikorycooper@gmail.com>
2018-02-17 12:38:05 -0800

    commited changes from comments
    unwrapped from inside method,
    formatting

--- converter.js
+++ converter.js
@@ -1,21 +1,19 @@
-const csv = require('csvtojson');
 const path = require('path');
 const fs = require('fs');
 
-const converter = () => {
-    var jsonList = [];
-    csv()
-            .fromFile('customer-data (Autosaved).csv')
+var jsonList = [];
+const csv = require('csvtojson');
+csv()
+        .fromFile('customer-data (Autosaved).csv')
 
-            .on('json', (jsonObj, rowIndex) => {
-                jsonList[rowIndex] = jsonObj;
+        .on('json', (jsonObj, rowIndex) => {
+            jsonList[rowIndex] = jsonObj;
 
-            })
-            .on('done', () => {
-                console.log('done');
-                fs.writeFileSync('customer-data.json', JSON.stringify(jsonList, null, 2));
-            });
-};
+        })
+        .on('done', () => {
+            console.log('done');
+            fs.writeFileSync('customer-data.json', JSON.stringify(jsonList, null, 2));
+        });
 
-converter();
+
 
